4217657bafc5159bcaa90b041b4b375d960289cf
Pablo-Recio <93.pablo.recio@gmail.com>
2023-11-19 09:58:12 +1100

    Updates

--- .vdoc.r
+++ .vdoc.r
@@ -89,11 +89,36 @@ data_associative <- data %>%
   ungroup()  %>% 
     mutate(temp = gsub("[AB]_", "", trt),
           cort = gsub("_[2][38]", "", trt))  %>% 
-    mutate(group = factor(group, levels = c("R_B", "B_R"))) %>%
+    mutate(group = factor(group,
+     levels = c("R_B", "B_R"), 
+     labels=c("R_B"="Red", "B_R"="Blue"))) %>%
+    mutate(temp = factor(temp,
+     levels = c("23", "28"), 
+     labels=c("23"="Cold", "28"="Hot"))) %>%
+    mutate(cort = factor(cort,
+     levels = c("A", "B"), 
+     labels=c("A"="Control", "B"="CORT"))) %>%
   data.frame()
 
-# Standarize data by trial (i.e. make the first trial where each individual participated their trial 1)
+data_reversal <- data %>%
+  group_by(lizard_id) %>%
+    filter(sum(is.na(FC_reversal)) <= 15) %>%
+   ungroup()  %>% 
+    mutate(temp = gsub("[AB]_", "", trt),
+          cort = gsub("_[2][38]", "", trt))  %>% 
+    mutate(group = factor(group,
+     levels = c("R_B", "B_R"), 
+     labels=c("R_B"="Red", "B_R"="Blue"))) %>%
+    mutate(temp = factor(temp,
+     levels = c("23", "28"), 
+     labels=c("23"="Cold", "28"="Hot"))) %>%
+    mutate(cort = factor(cort,
+     levels = c("A", "B"), 
+     labels=c("A"="Control", "B"="CORT"))) %>%
+  data.frame() 
 
+
+# Standarize data by trial (i.e. make the first trial where each individual participated their trial 1)
 data_associative <- data_associative %>%
   group_by(lizard_id) %>%
   mutate(
@@ -108,6 +133,9 @@ write.csv(data_associative, file= "./output/Checking/data_associative.csv")
 deli_associative <- data_associative %>% filter (species == "delicata")
 guich_associative <- data_associative %>% filter (species == "guichenoti")
 
+# Split data by species
+deli_reversal <- data_reversal %>% filter (species == "delicata")
+guich_reversal <- data_reversal %>% filter (species == "guichenoti")
 #
 #
 #
@@ -134,7 +162,7 @@ emmeans(GMM_guich, pairwise ~ temp*cort*Associative_Trial)
 #
 #
 #
-#| label: models2
+#| label: modelsasso
 #| echo: false
 #| warning: false
 
@@ -142,20 +170,7 @@ emmeans(GMM_guich, pairwise ~ temp*cort*Associative_Trial)
   deli_mod <- brm(FC_associative ~ cort*temp*Associative_Trial + group*Associative_Trial + (1 + Associative_Trial|lizard_id), data = deli_associative, family = bernoulli(link = "logit"), chains = 4, cores = 4, iter = 2000, warmup = 1000, control = list(adapt_delta = 0.99))
   summary(deli_mod)
   plot(deli_mod)
-flextable_summary <- flextable(deli_mod) %>%
-  set_table_properties(width = .8, layout = "autofit") %>%
-  theme_zebra() %>%
-tidy_summary <- data.frame(term = rownames(fixed_effects),
-                            estimate = fixed_effects,
-                            conf.low = summary_table$ci[,"lower"],
-                            conf.high = summary_table$ci[,"upper"])
-flextable::set_header_labels(
-    term = "Term",
-    estimate = "Estimate",
-    conf.low = "Lower CI",
-    conf.high = "Upper CI"
-  ) %>%
-  flextable::rename(columns = "term" ~ "Fixed Effects")
+
 # Bayesian model guichenoti
   guich_mod <- brm(FC_associative ~ cort*temp*Associative_Trial + group*Associative_Trial + (1 + Associative_Trial|lizard_id), data = guich_associative, family = bernoulli(link = "logit"), chains = 4, cores = 4, iter = 2000, warmup = 1000, control = list(adapt_delta = 0.99))
   summary(guich_mod)
@@ -178,23 +193,60 @@ dharma(GMM_deli2)
 #
 #
 #
+#| label: modelsrev
+#| echo: false
+#| warning: false
+
+# Bayesian model delicata
+  deli_rev <- brm(FC_reversal ~ cort*temp*trial_reversal + group*trial_reversal + (1 + trial_reversal|lizard_id), data = deli_reversal, family = bernoulli(link = "logit"), chains = 4, cores = 4, iter = 2000, warmup = 1000, control = list(adapt_delta = 0.99))
+  summary(deli_rev)
+  plot(deli_rev)
+
+# Bayesian model guichenoti
+  guich_rev <- brm(FC_reversal ~ cort*temp*trial_reversal + group*trial_reversal + (1 + trial_reversal|lizard_id), data = guich_reversal, family = bernoulli(link = "logit"), chains = 4, cores = 4, iter = 2000, warmup = 1000, control = list(adapt_delta = 0.99))
+  summary(guich_rev)
+  plot(guich_rev)
+# R2
+bayes_R2(deli_rev)
+bayes_R2(guich_rev)
+
+# Extract posteriors
+posterior <- posterior_samples(mod, pars = "^b")
+plyr::ldply(lapply(posterior, mean))
+plyr::ldply(lapply(posterior, function(x) quantile(x, c(0.025, 0.975))))
+temp23 <- c(posterior[,"b_Intercept"], (posterior[,"b_Intercept"]+ posterior[,"b_trtB_23"]))
+temp28 <- c((posterior[,"b_Intercept"] + posterior[,"b_trtB_28"]), posterior[,"b_trtB_23"])
+
+pmcmc(temp23 - temp28)
+dharma(GMM_deli2)
+
+
+#
+#
+#
 #| label: plots
 #| echo: false
 #| warning: false
  
-# Plot delicata
+# Plot delicata associative
 plot_deli <- ggplot(deli_associative, aes(x = Associative_Trial, y = FC_associative, color=interaction(cort,temp))) +  
-  geom_smooth(method = "glm", formula = y ~ x, method.args = list(family = "binomial"),na.action = na.exclude) +
+  geom_smooth(method = "glm", formula = y ~ x, method.args = list(family = "binomial")) +
   facet_grid(as.factor(deli_associative$group)) + 
-  labs(y = "Colour Association Task", x = "Trial")  
+  labs(y = ""Probability of correct choice"", x = "Trial")  
 print(plot_deli)
 
-# Plot guichenoti
+# Plot guichenoti associative
 plot_guich <- ggplot(guich_associative, aes(x = Associative_Trial, y = FC_associative, color=interaction(cort,temp))) +  
   geom_smooth(method = "glm", formula = y ~ x, method.args = list(family = "binomial"),na.action = na.exclude) +
   facet_grid(as.factor(guich_associative$group)) + 
-  labs(y = "Colour Association Task", x = "Trial") 
+  labs(y = "Probability of correct choice", x = "Trial") 
 print(plot_guich)
+
+
+# Plot delicata reversal
+
+# Plot guichenoti reversal
+
 #
 #
 #
