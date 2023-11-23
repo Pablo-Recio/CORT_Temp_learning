929b3aa52ce7f33e1bac5ba8cc455205a3e8490f
Pablo-Recio <93.pablo.recio@gmail.com>
2023-11-23 22:36:20 +1100

    Updates

--- R/1_data_process.R
+++ R/1_data_process.R
@@ -3,16 +3,47 @@
 ####################################
 
 ## Load packages
-	intstall.packages("pacman")
-	pacman::p_load(tidyverse, readxl)
+	install.packages("pacman")
+	pacman::p_load(tidyverse, readxl, ggplot2, dplyr, magrittr)
 
 ## Load data	
-	              activ_files <- list.files("./data/Activity/")
-	       data_activity_list <- lapply(activ_files, function(x) read_xlsx(paste0("./data/Activity/", x))[-c(1:3),])
-	names(data_activity_list) <- activ_files
+data<-read.csv("./data/learning.csv")
+
+data<- data %>%
+    mutate(temp = gsub("[AB]_", "", trt),
+          cort = gsub("_[2][38]", "", trt))  %>% 
+    mutate(temp = factor(temp,
+     levels = c("23", "28"), 
+     labels=c("23"="Cold", "28"="Hot"))) %>%
+    mutate(cort = factor(cort,
+     levels = c("A", "B"), 
+     labels=c("A"="Control", "B"="CORT"))) %>%
+	mutate(species = factor(species,
+	 labels=c("delicata"="L. delicata", "guichenoti"="L. guichenoti")))
+  data.frame()
+
+
+## Histogram ages by treatment
+hist<-ggplot(data, aes(x = age.start, y = ..density..)) +  
+  geom_density(alpha = 0.7,
+  aes(fill = interaction(cort, temp))) +
+  scale_fill_manual(values = c("CORT.Cold"="darkblue", "Control.Cold"="cyan", "CORT.Hot"="black", "Control.Hot"="grey"),
+    labels=c("CORT-Cold (n=11)", "Control-Cold (n=12)", "CORT-Hot (n=11)", "Control-Hot (n=10)")
+  ) +
+  facet_wrap(~species, scales = "free_y", ncol = 2) +  
+  theme(strip.background = element_blank()) +
+  labs(y="", x = "Age") +
+  theme_classic() +
+  theme(
+    axis.title = element_text(size = 18, family = "sans"),  
+    axis.text = element_text(size = 10, family = "sans"),  
+    legend.title = element_text(size = 14, family = "sans"), 
+    legend.text = element_text(size = 12, family = "sans")  
+  )
+print(hist)
+ggsave("./output/figures/hist.png", plot=hist, width = 30, height = 12, units = "cm", dpi = 3000) 
 
 ## Merge
-	data_activity <- dplyr::bind_rows(data_activity_list, .id = "id")
 
 
 
